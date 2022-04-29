fe56190be288a5e0757691fd4aced1daa058dfc8
yo-yona <svetlanya05@mail.ru>
2022-04-29 13:29:58 +0300

    Добавьте файлы проекта.

---
+++ SquareCalculatorLib/Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace SquareCalculatorLib
+{
+    public static class Calculator
+    {
+        public static double CalculateSquareOf(params int[] measurements)
+        {
+            switch (measurements.Length)
+            {
+                case 1:
+                    {
+                        Circle circle = new Circle(measurements[0]);
+                        Console.WriteLine("Circle");
+                        return circle.GetArea();
+                    }
+
+                case 3:
+                    {
+                        Triangle triangle = new Triangle(measurements);
+                        Console.WriteLine("Triangle");
+                        return triangle.GetArea();
+                    }
+
+                default: throw new Exception("Not supported");
+
+            }
+        }
+        public static bool IsThisRight(params int[] sides)
+        {
+            if (sides.Length == 3)
+            {
+                Triangle triangle = new Triangle(sides);
+                return triangle.IsRight();
+            }
+            else throw new ArgumentException("You didn't pass sufficient information about the triangle");
+        }
+    }
+}
---
+++ SquareCalculatorLib/Figure.cs
@@ -0,0 +1,51 @@
+﻿namespace SquareCalculatorLib
+{
+    internal abstract class Figure
+    {
+        public int[] Measurements { get; set; }
+        public double Perimeter { get; set; }
+        public double Area { get; set; }
+        public Figure(params int[] measurements)
+        {
+            Measurements = new int[measurements.Length];
+            measurements.CopyTo(Measurements, 0);
+            CalcPerimeter();
+            CalcArea();
+        }
+        public virtual void CalcPerimeter() => Perimeter = Measurements.Sum();
+        public abstract void CalcArea();
+        public virtual double GetArea() => Area;
+        public double GetPerimeter() => Perimeter;
+    }
+
+    class Triangle : Figure
+    {
+        private int Hypotenuse;
+        public Triangle(params int[] sides) : base(sides) { Hypotenuse = Measurements.Max();}
+        public override void CalcArea()
+        {
+            var halfperim = Perimeter / 2;
+            Area =  Math.Sqrt(halfperim * (halfperim - Measurements[0]) * (halfperim - Measurements[1]) * (halfperim - Measurements[2]));
+        }
+        public override double GetArea()
+        {
+            if (double.IsNaN(Area) || Area == 0) Console.WriteLine("Такого треугольника не существует.");
+            return base.GetArea();
+        }
+        public bool IsRight()
+        {
+            int[] othersides = Measurements.Where(x => x != Hypotenuse).ToArray();
+            if (Math.Sqrt(Math.Pow(othersides[0], 2) + Math.Pow(othersides[1], 2)) == Hypotenuse) 
+                return true;
+            else return false;
+        }
+    }
+
+    class Circle : Figure
+    {
+        public Circle(int radius) : base(radius) { }
+
+        public new void CalcPerimeter() => Perimeter = Measurements[0] * 2 * Math.PI;
+        public override void CalcArea() => Area = Measurements[0] * Measurements[0] * Math.PI;
+    }
+}
