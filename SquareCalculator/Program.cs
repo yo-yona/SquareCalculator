a50546fd47ebb040348cbf4426f944dbb621e667
yo-yona <svetlanya05@mail.ru>
2022-04-30 22:26:15 +0300

    Rearranged

--- SquareCalculator/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using SquareCalculatorLib;
-
-Console.WriteLine(Calculator.CalculateSquareOf(2));
-Console.WriteLine(Calculator.CalculateSquareOf(3, 4, 5));
-Console.WriteLine(Calculator.IsThisRight(3, 4, 5));
-Console.WriteLine(Calculator.CalculateSquareOf(0, 0));
